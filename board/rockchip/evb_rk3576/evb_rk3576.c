0951573c7b0b0e52eda85efd3efffa44ef950e0e
Nascs Fang <nascs@radxa.com>
2025-04-25 18:11:53 +0800

    board: rockchip: evb_rk3576: select rock-4d with bom_id
    Signed-off-by: Nascs Fang <nascs@radxa.com>
    Co-authored-by: ZHANG Yuntian <yt@radxa.com>

--- board/rockchip/evb_rk3576/evb_rk3576.c
+++ board/rockchip/evb_rk3576/evb_rk3576.c
@@ -15,6 +15,7 @@
 
 #define SARADC_ADDR	"adc@2ae00000"
 #define HW_ID_CHANNEL	2
+#define BOM_ID_CHANNEL	5
 
 #define countof(x) (sizeof(x) / sizeof(x[0]))
 
@@ -22,6 +23,8 @@ struct variant_def {
 	char *compatible;
 	unsigned int hw_id_lower_bound;
 	unsigned int hw_id_upper_bound;
+	unsigned int bom_id_lower_bound;
+	unsigned int bom_id_upper_bound;
 	char *fdtfile;
 };
 
@@ -100,16 +103,16 @@ int board_usb_cleanup(int index, enum usb_init_type init)
 
 #ifdef CONFIG_ID_EEPROM
 static struct variant_def variants[] = {
-	{"radxa,cm4-io", 0, 40, "rockchip/rk3576-radxa-cm4-io.dtb"},
-	{"radxa,rock-4d-spi", 380, 460, "rockchip/rk3576-rock-4d-spi.dtb"},
-	{"radxa,nx4-nano-c200", 780, 860, "rockchip/rk3576-radxa-nx4-nano-c200.dtb"},
-	{"radxa,rock-4d", 1190,1270, "rockchip/rk3576-rock-4d.dtb"},
+	{"radxa,cm4-io", 0, 40, 0, -1, "rockchip/rk3576-radxa-cm4-io.dtb"},
+	{"radxa,rock-4d", 380, 460, 0, 40, "rockchip/rk3576-rock-4d.dtb"},
+	{"radxa,rock-4d-spi", 380, 460, 2000, 2080, "rockchip/rk3576-rock-4d-spi.dtb"},
+	{"radxa,nx4-nano-c200", 780, 860, 0, -1, "rockchip/rk3576-radxa-nx4-nano-c200.dtb"},
 };
 
 static void set_fdtfile(void)
 {
 	int i, ret;
-	unsigned int hw_id;
+	unsigned int hw_id, bom_id;
 	struct variant_def *v;
 
 	ret = adc_channel_single_shot(SARADC_ADDR, HW_ID_CHANNEL, &hw_id);
@@ -117,16 +120,24 @@ static void set_fdtfile(void)
 		pr_err("%s: adc_channel_single_shot fail for HW_ID: %i!\n", __func__, ret);
 		return;
 	}
-	
+	ret = adc_channel_single_shot(SARADC_ADDR, BOM_ID_CHANNEL, &bom_id);
+	if (ret) {
+		pr_err("%s: adc_channel_single_shot fail for BOM_ID: %i!\n", __func__, ret);
+		return;
+	}
+
 	for(i = 0; i < countof(variants); i++) {
 		v = &variants[i];
-		if (hw_id >= v->hw_id_lower_bound && hw_id <= v->hw_id_upper_bound) {
-			printf("Found matching variant %s for hw_id 0x%x, setting fdtfile to %s\n", v->compatible, hw_id, v->fdtfile);
+		if (hw_id >= v->hw_id_lower_bound &&
+		    hw_id <= v->hw_id_upper_bound &&
+		    bom_id >= v->bom_id_lower_bound &&
+		    bom_id <= v->bom_id_upper_bound) {
+			printf("Found matching variant %s for hw_id 0x%x and bom_id 0x%x setting fdtfile to %s\n", v->compatible, hw_id, bom_id, v->fdtfile);
 			env_set("fdtfile", v->fdtfile);
 			break;
 		} else {
-			printf("Checking variant: %s, hw_id 0x%x not in range [0x%x, 0x%x]\n",
-			       v->compatible, hw_id, v->hw_id_lower_bound, v->hw_id_upper_bound);
+			printf("Checking variant: %s, hw_id 0x%x not in range [0x%x, 0x%x], bom_id 0x%x not in range [0x%x, 0x%x]\n",
+			       v->compatible, hw_id, v->hw_id_lower_bound, v->hw_id_upper_bound, bom_id, v->bom_id_lower_bound, v->bom_id_upper_bound);
 		}
 	}
 }
