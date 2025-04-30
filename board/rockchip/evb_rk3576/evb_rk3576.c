eddf333117dafbb32858a71851e67172d4041bc4
SongJun Li <lisongjun@radxa.com>
2025-04-30 17:06:56 +0800

    board: rockchip: evb_rk3576: rename nx4-nano-c200 to nx4-orin-c200
    Signed-off-by: SongJun Li <lisongjun@radxa.com>

--- board/rockchip/evb_rk3576/evb_rk3576.c
+++ board/rockchip/evb_rk3576/evb_rk3576.c
@@ -107,7 +107,7 @@ static struct variant_def variants[] = {
 	{"radxa,cm4-io", 0, 40, 0, -1, "rockchip/rk3576-radxa-cm4-io.dtb"},
 	{"radxa,rock-4d", 380, 460, 0, 40, "rockchip/rk3576-rock-4d.dtb"},
 	{"radxa,rock-4d-spi", 380, 460, 2000, 2080, "rockchip/rk3576-rock-4d-spi.dtb"},
-	{"radxa,nx4-nano-c200", 780, 860, 0, -1, "rockchip/rk3576-radxa-nx4-nano-c200.dtb"},
+	{"radxa,nx4-orin-c200", 780, 860, 0, -1, "rockchip/rk3576-radxa-nx4-orin-c200.dtb"},
 };
 
 static void set_fdtfile(void)
