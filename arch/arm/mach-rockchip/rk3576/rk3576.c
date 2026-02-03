radxa/u-boot

commit 895dde418203814e1c7fd5c1af5dfea0c72ea953

rockchip: board: fixup gmac0 for rk3576s
Signed-off-by: Weixin Zhou <zwx@rock-chips.com>
Change-Id: I74ae26d7e82e867b874d8704ae88e4f4bef59f14

arch/arm/mach-rockchip/rk3576/rk3576.c
@@ -19,6 +19,8 @@
 #include <asm/arch/ioc_rk3576.h>
 #include <asm/arch/rockchip_smccc.h>
 #include <asm/system.h>
+#include <asm/arch/vendor.h>
+#include <optee_include/OpteeClientInterface.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -470,3 +472,37 @@ int rk_board_dm_fdt_fixup(const void *blob)
 }
 #endif
 
+/* @brief: Fix up the device tree of gmac0
+ *
+ * This function enables GMAC0 after verifying the license.
+ *
+ * @param blob Pointer to the device tree blob
+ * @return 0 on success
+ **/
+#if defined(CONFIG_ROCKCHIP_VENDOR_PARTITION)
+int rk_board_fdt_fixup(const void *blob)
+{
+	char licence_str[1024] = {0};
+	int ret, size, node;
+
+	size = vendor_storage_read(MULTI_MODULE_KEY_ID, licence_str, 1024);
+	if (size > 0) {
+		ret = trusty_verify_config_ip(licence_str);
+		if (!ret)
+			printf("gmac0 can be enabled safely\n");
+		else
+			return 0;
+
+		node = fdt_path_offset(blob, "/ethernet@2a220000");
+		if (node < 0) {
+			printf("Error: /ethernet@2a220000 cannot find node\n");
+		} else {
+			ret = fdt_setprop_string((void *)blob, node, "status", "okay");
+			if (ret < 0)
+				printf("Error: /ethernet@2a220000 cannot set status property\n");
+		}
+	}
+
+	return 0;
+}
+#endif

arch/arm/include/asm/arch-rockchip/vendor.h
@@ -25,6 +25,7 @@
 #define LAN_RGMII_DL_ID			16
 #define EINK_VCOM_ID			17
 #define FIRMWARE_VER_ID			18
+#define MULTI_MODULE_KEY_ID		1068
 
 struct vendor_item {
 	u16  id;

commit 4918e1373047fa145342242bd901e71a9effabfd

rockchip: rk3576: run trusty_verify_config_ip when CONFIG_OPTEE_CLIENT is defined
Fixes: 895dde418203 ("rockchip: board: fixup gmac0 for rk3576s")

Signed-off-by: Stephen Chen <stephen@radxa.com>

arch/arm/mach-rockchip/rk3576/rk3576.c
@@ -513,11 +513,13 @@ int rk_board_fdt_fixup(const void *blob)
 
 	size = vendor_storage_read(MULTI_MODULE_KEY_ID, licence_str, 1024);
 	if (size > 0) {
+#if defined(CONFIG_OPTEE_CLIENT)
 		ret = trusty_verify_config_ip(licence_str);
 		if (!ret)
 			printf("gmac0 can be enabled safely\n");
 		else
 			return 0;
+#endif
 
 		node = fdt_path_offset(blob, "/ethernet@2a220000");
 		if (node < 0) {